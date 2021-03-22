31ec301a774581c113b485110e4546ecf0883ed2
Samantha Jordan <shinyguns@gmail.com>
2021-03-22 17:49:56 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+    const content = document.getElementById("text")
+    text.innerHTML = "This is really cool!"
+})
